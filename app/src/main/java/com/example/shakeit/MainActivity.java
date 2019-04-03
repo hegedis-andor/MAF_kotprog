fee10b25e84e877c4296ae96f893ee1e06258bf4
Hegedus Andor <hegedis.andor@gmail.com>
2019-04-03 19:35:32 +0200

    orientation is locked for portrait

--- app/src/main/java/com/example/shakeit/MainActivity.java
+++ app/src/main/java/com/example/shakeit/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.shakeit;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.counter = 1;
     }
+
+    protected void changeBackground(View view)
 }
