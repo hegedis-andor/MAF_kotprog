317570eab90a8e133b53dc5970758b67dcea6f35
Hegedus Andor <hegedis.andor@gmail.com>
2019-04-06 15:06:45 +0200

    Sound And Notification are ready
    AsyncTask class deleted

--- app/src/main/java/com/example/shakeit/SensorDataUtil.java
+++ app/src/main/java/com/example/shakeit/SensorDataUtil.java
@@ -1,15 +1,21 @@
 package com.example.shakeit;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -22,16 +28,18 @@ public class SensorDataUtil implements SensorEventListener {
     private CountDownTimer countDownTimer;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
-    private MediaPlayer mediaPlayer;
-
+    private MediaPlayer nononoMP;
+    private MediaPlayer plsNoMP;
+    private boolean isNotified;
 
     public SensorDataUtil(@NonNull Context context) {
         this.context = context;
         this.sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         this.sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         this.sensorManager.registerListener(this, sensorAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-        this.mediaPlayer = mediaPlayer.create(context, R.raw.nonosound);
-        this.countDownTimer = new CountDownTimer(1200, 300) {
+        this.nononoMP = nononoMP.create(context, R.raw.nononofx);
+        this.plsNoMP = plsNoMP.create(context, R.raw.pleasenofx);
+        this.countDownTimer = new CountDownTimer(2200, 300) {
             @Override
             public void onTick(long millisUntilFinished) { }
 
@@ -47,6 +55,7 @@ public class SensorDataUtil implements SensorEventListener {
                 Log.e("Counter: " , String.valueOf(sickCounter));
             }
         }.start();
+        this.isNotified = false;
     }
 
     @Override
@@ -73,29 +82,41 @@ public class SensorDataUtil implements SensorEventListener {
                 if (speed > 4500) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 60000 ?  60000 : sickCounter+3500;
+                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+4000;
 
                 } else if (speed > 3000) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 60000 ?  60000 : sickCounter+2000;
+                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+2000;
 
                 } else if (speed > 1500) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 60000 ?  60000 : sickCounter+500;
+                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+500;
 
                 }
 
-                if(sickCounter >= 60000) {
-                    if (!this.mediaPlayer.isPlaying()) this.mediaPlayer.start();
+                // Logic of state change, notification and sound
+                if (sickCounter >= 100000) {
+                    if (!this.plsNoMP.isPlaying()) this.plsNoMP.start();
+                }
+                else if (sickCounter >= 80000) {
+                    if (!this.isNotified) {
+                        noNoNotify();
+                        this.isNotified = true;
+                    }
+                }
+                else if(sickCounter >= 60000) {
+                    if (!this.nononoMP.isPlaying() && speed > 1500) this.nononoMP.start();
+                    if (this.isNotified) this.isNotified = false;
                     sendStateChange("3");
                 }
                 else if (sickCounter  >= 32000)  {
                     sendStateChange("2");
                 }
                 else if (sickCounter >= 10000)  {
-                    if (this.mediaPlayer.isPlaying()) this.mediaPlayer.pause();
+                    if (this.nononoMP.isPlaying()) this.nononoMP.pause();
+                    if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
                     sendStateChange("1");
                 }
                 Log.e("Counter: " , String.valueOf(sickCounter));
@@ -126,8 +147,41 @@ public class SensorDataUtil implements SensorEventListener {
     public void pauseAccelerometerListening() {
         this.sensorManager.unregisterListener(this);
         this.countDownTimer.cancel();
-        if (this.mediaPlayer.isPlaying()) this.mediaPlayer.pause();
+        if (this.nononoMP.isPlaying()) this.nononoMP.pause();
+        if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
     }
+
+    public void noNoNotify() {
+        CharSequence channelName = null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelName = "shakeNotificationChan";
+            String description = "Intense shake notification.";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(channelName.toString(), channelName, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context, channelName != null ? channelName.toString() : null)
+                        .setContentTitle("Stop, Stop, Stop!")
+                        .setContentText("Please Stoooooppppppppppp!!!")
+                        .setSmallIcon(R.drawable.sick)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
+                            R.drawable.sick));
+
+
+        NotificationManagerCompat notifcationManager = NotificationManagerCompat.from(context);
+        notifcationManager.notify(0, builder.build());
+    }
+
+
 
 
     private void restartCountdownTimer() {
