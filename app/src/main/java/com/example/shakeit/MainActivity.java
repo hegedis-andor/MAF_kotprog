b614f8a1cfb653f875ba128bf9b362aea50a4fc4
Hegedus Andor <hegedis.andor@gmail.com>
2019-04-12 10:57:42 +0200

    clean up

--- app/src/main/java/com/example/shakeit/MainActivity.java
+++ app/src/main/java/com/example/shakeit/MainActivity.java
@@ -5,20 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity /*implements LoaderManager.LoaderCallbacks<Void>*/ {
+public class MainActivity extends AppCompatActivity  {
 
-    //private SensorAsyncTaskLoader sensorAsyncTaskLoader;
     private SensorDataUtil sensorDataUtil;
     private ConstraintLayout constraintLayout;
     private ImageView emojiImageView;
@@ -35,7 +31,6 @@ public class MainActivity extends AppCompatActivity /*implements LoaderManager.L
 
 
         this.sensorDataUtil = new SensorDataUtil(getApplicationContext());
-        //this.sensorAsyncTaskLoader = new SensorAsyncTaskLoader(getApplicationContext());
 
         LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver,
                 new IntentFilter("backgroundStateChange"));
@@ -56,19 +51,19 @@ public class MainActivity extends AppCompatActivity /*implements LoaderManager.L
     public void changeBackground(int state) {
         switch (state) {
             case 0:
-                constraintLayout.setBackgroundColor(getResources().getColor(R.color.START_GREEN));
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.START_GREEN));
                 emojiImageView.setBackgroundResource(R.drawable.happy);
                 break;
             case 1:
-                constraintLayout.setBackgroundColor(getResources().getColor(R.color.MID_YELLOW));
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_YELLOW));
                 emojiImageView.setBackgroundResource(R.drawable.surprised);
                 break;
             case 2:
-                constraintLayout.setBackgroundColor(getResources().getColor(R.color.MID_ORANGE));
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_ORANGE));
                 emojiImageView.setBackgroundResource(R.drawable.confused);
                 break;
             case 3:
-                constraintLayout.setBackgroundColor(getResources().getColor(R.color.HIGH_RED));
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.HIGH_RED));
                 emojiImageView.setBackgroundResource(R.drawable.confused);
                 break;
         }
@@ -87,18 +82,5 @@ public class MainActivity extends AppCompatActivity /*implements LoaderManager.L
         this.sensorDataUtil.pauseAccelerometerListening();
         Log.e("Pause", "sensordata");
     }
-/*
-    @NonNull
-    @Override
-    public Loader<Void> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return new SensorAsyncTaskLoader(getApplicationContext());
-    }
 
-    @Override
-    public void onLoadFinished(@NonNull Loader<Void> loader, String s) { }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Void> loader) {
-
-    }*/
 }
