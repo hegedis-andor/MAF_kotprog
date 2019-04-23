d3e9b0bb0e723362255fb06dfb4011fba3367610
hegedis <hegedis.andor@gmail.com>
2019-04-23 13:33:20 +0200

    notification and mediaplayers moved to MainActivity

--- app/src/main/java/com/example/shakeit/MainActivity.java
+++ app/src/main/java/com/example/shakeit/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.shakeit;
 
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity  {
     private SensorDataUtil sensorDataUtil;
     private ConstraintLayout constraintLayout;
     private ImageView emojiImageView;
+    private MediaPlayer nononoMP;
+    private MediaPlayer plsNoMP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity  {
 
         changeBackground(0);
 
+        this.nononoMP = MediaPlayer.create(this, R.raw.nononofx);
+        this.plsNoMP = MediaPlayer.create(this, R.raw.pleasenofx);
 
         this.sensorDataUtil = new SensorDataUtil(getApplicationContext());
 
@@ -50,37 +63,86 @@ public class MainActivity extends AppCompatActivity  {
     //UI background and emoji changes by the received state
     public void changeBackground(int state) {
         switch (state) {
+            case 50:
+                if (!this.plsNoMP.isPlaying()) this.plsNoMP.start();
+                break;
+            case 40:
+                noNoNotify();
+                break;
+            case 31:
+                if(!this.nononoMP.isPlaying()) this.nononoMP.start();
+                break;
+            case 30:
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.HIGH_RED));
+                emojiImageView.setBackgroundResource(R.drawable.confused);
+                break;
+            case 20:
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_ORANGE));
+                emojiImageView.setBackgroundResource(R.drawable.confused);
+                break;
+            case 10:
+                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_YELLOW));
+                emojiImageView.setBackgroundResource(R.drawable.surprised);
+                if (this.nononoMP.isPlaying()) this.nononoMP.pause();
+                if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
+                break;
             case 0:
                 constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.START_GREEN));
                 emojiImageView.setBackgroundResource(R.drawable.happy);
                 break;
-            case 1:
-                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_YELLOW));
-                emojiImageView.setBackgroundResource(R.drawable.surprised);
-                break;
-            case 2:
-                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.MID_ORANGE));
-                emojiImageView.setBackgroundResource(R.drawable.confused);
-                break;
-            case 3:
-                constraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.HIGH_RED));
-                emojiImageView.setBackgroundResource(R.drawable.confused);
-                break;
         }
+    }
+
+
+
+    public void noNoNotify() {
+        CharSequence channelName = null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channelName = "shakeNotificationChan";
+            String description = "Intense shake notification.";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(channelName.toString(), channelName, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = this.getSystemService(NotificationManager.class);
+
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, new Intent(), 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                this, channelName != null ? channelName.toString() : null)
+                .setContentTitle("Stop, Stop, Stop!")
+                .setContentText("Please Stoooooppppppppppp!!!")
+                .setSmallIcon(R.drawable.sick)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.drawable.sick));
+
+
+        NotificationManagerCompat notifcationManager = NotificationManagerCompat.from(this);
+        notifcationManager.notify(0, builder.build());
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        this.sensorDataUtil.startAccelerometerListening();
-        Log.e("resume", "sensordata");
+        this.sensorDataUtil.startAccelerometerAndCountDown();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        this.sensorDataUtil.pauseAccelerometerListening();
-        Log.e("Pause", "sensordata");
+        this.sensorDataUtil.pauseAccelerometerAndCountDown();
+        if (this.nononoMP.isPlaying()) this.nononoMP.pause();
+        if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
     }
 
 }
--- app/src/main/java/com/example/shakeit/SensorDataUtil.java
+++ app/src/main/java/com/example/shakeit/SensorDataUtil.java
@@ -1,59 +1,45 @@
 package com.example.shakeit;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.support.annotation.NonNull;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 public class SensorDataUtil implements SensorEventListener {
 
     private Context context;
     private SensorManager sensorManager;
     private Sensor sensorAccelerometer;
-    private static int sickCounter = 0;
+    private static int counter = 0;
     private CountDownTimer countDownTimer;
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
-    private MediaPlayer nononoMP;
-    private MediaPlayer plsNoMP;
     private boolean isNotified;
 
-    public SensorDataUtil(@NonNull Context context) {
+    SensorDataUtil(@NonNull Context context) {
         this.context = context;
         this.sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         this.sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         this.sensorManager.registerListener(this, sensorAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
-        this.nononoMP = nononoMP.create(context, R.raw.nononofx);
-        this.plsNoMP = plsNoMP.create(context, R.raw.pleasenofx);
-        this.countDownTimer = new CountDownTimer(2200, 300) {
+        this.countDownTimer = new CountDownTimer(1200, 300) {
             @Override
             public void onTick(long millisUntilFinished) { }
 
             @Override
             public void onFinish() {
-                sickCounter = sickCounter > 20000 ? sickCounter-20000 : 0;
-                if (sickCounter == 0) {
+                counter = counter > 20000 ? counter -20000 : 0;
+                if (counter == 0) {
                     cancelCountdownTimer();
-                    sendStateChange("0");
+                    sendStateChange("0"); //happy state
                 } else {
                     restartCountdownTimer();
                 }
-                Log.e("Counter: " , String.valueOf(sickCounter));
             }
         }.start();
         this.isNotified = false;
@@ -78,49 +64,55 @@ public class SensorDataUtil implements SensorEventListener {
 
                 // speed of movement
                 float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
-
+//----------------------------------------------
                 //The bigger the speed the bigger the step of the sick counter
                 if (speed > 4500) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+4000;
+                    counter = counter >= 100000 ?  100000 : counter +4000;
 
                 } else if (speed > 3000) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+2000;
+                    counter = counter >= 100000 ?  100000 : counter +2000;
 
                 } else if (speed > 1500) {
                     this.countDownTimer.cancel();
                     this.countDownTimer.start();
-                    sickCounter = sickCounter >= 100000 ?  100000 : sickCounter+500;
+                    counter = counter >= 100000 ?  100000 : counter +500;
 
                 }
-
+//----------------------------------------------
                 // Logic of state change, notification and sound
-                if (sickCounter >= 100000) {
-                    if (!this.plsNoMP.isPlaying()) this.plsNoMP.start();
+                if (counter >= 100000) {
+                    //state -> 50 sick background, second sound
+                    sendStateChange("50");
                 }
-                else if (sickCounter >= 80000) {
+                else if (counter >= 80000) {
+                    //state 40 -> sick background, notification
                     if (!this.isNotified) {
-                        noNoNotify();
+                        sendStateChange("40"); //notification
                         this.isNotified = true;
                     }
                 }
-                else if(sickCounter >= 60000) {
-                    if (!this.nononoMP.isPlaying() && speed > 1500) this.nononoMP.start();
+                else if(counter >= 60000) {
+                    //start sound if not playing currently
+                    if (speed > 1500)
+                        sendStateChange("31");
                     if (this.isNotified) this.isNotified = false;
-                    sendStateChange("3");
+                    //state 30 -> sick background, first sound
+                    sendStateChange("30");
                 }
-                else if (sickCounter  >= 32000)  {
-                    sendStateChange("2");
+                else if (counter >= 32000)  {
+                    //state -> 20 confused background
+                    sendStateChange("20");
                 }
-                else if (sickCounter >= 10000)  {
-                    if (this.nononoMP.isPlaying()) this.nononoMP.pause();
-                    if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
-                    sendStateChange("1");
+                else if (counter >= 10000)  {
+                    //state 10 -> surprised background
+                    sendStateChange("10");
                 }
-
+                //state 0 -> happy background ( in countdown timer)
+//----------------------------------------------
                 last_x = x;
                 last_y = y;
                 last_z = z;
@@ -135,59 +127,16 @@ public class SensorDataUtil implements SensorEventListener {
         LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
     }
 
-
-
-    public void startAccelerometerListening() {
+    void startAccelerometerAndCountDown() {
         this.sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         this.countDownTimer.start();
 
     }
 
-    public void pauseAccelerometerListening() {
+    void pauseAccelerometerAndCountDown() {
         this.sensorManager.unregisterListener(this);
         this.countDownTimer.cancel();
-        if (this.nononoMP.isPlaying()) this.nononoMP.pause();
-        if (this.plsNoMP.isPlaying()) this.plsNoMP.pause();
     }
-
-    public void noNoNotify() {
-        CharSequence channelName = null;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            channelName = "shakeNotificationChan";
-            String description = "Intense shake notification.";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-
-            NotificationChannel channel = new NotificationChannel(channelName.toString(), channelName, importance);
-            channel.setDescription(description);
-
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(this.context, 0, new Intent(), 0);
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                context, channelName != null ? channelName.toString() : null)
-                        .setContentTitle("Stop, Stop, Stop!")
-                        .setContentText("Please Stoooooppppppppppp!!!")
-                        .setSmallIcon(R.drawable.sick)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setAutoCancel(true)
-                        .setContentIntent(pendingIntent)
-                        .setLargeIcon(BitmapFactory.decodeResource(context.getResources(),
-                            R.drawable.sick));
-
-
-        NotificationManagerCompat notifcationManager = NotificationManagerCompat.from(context);
-        notifcationManager.notify(0, builder.build());
-    }
-
-
-
 
     private void restartCountdownTimer() {
         this.countDownTimer.start();
